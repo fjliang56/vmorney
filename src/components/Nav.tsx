65d89d4f8d9ee999df2aeea041c8dad6e1f24d2d
fjliang56 <86655295+fjliang56@users.noreply.github.com>
2021-08-06 17:12:01 +0800

    当前路由图标自动高亮

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,28 +1,40 @@
 import styled from 'styled-components';
-import {Link} from 'react-router-dom';
+import {Link,NavLink} from 'react-router-dom';
 import React from 'react';
 import Icon from './Icon';
 
 
 const NavWrapper = styled.nav`
   line-height: 24px;
-  box-shadow: 0 0 3px rgba(0,0,0,0.25);
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+
   > ul {
-    padding:0; margin:0;
+    padding: 0;
+    margin: 0;
     display: flex;
+
     > li {
-      list-style-type:none;
+      list-style-type: none;
       width: 33.333%;
       text-align: center;
+
       > a {
         display: flex;
         flex-direction: column;
         padding: 4px 0;
         justify-content: center;
         align-items: center;
+
         .icon {
           width: 30px;
           height: 30px;
+        }
+
+        &.selected {
+          color: #0d4470;
+          .icon{
+            fill: #0d4470;
+          }
         }
       }
     }
@@ -34,16 +46,16 @@ const Nav = () => {
       <NavWrapper>
           <ul>
               <li>
-                  <Link to="/tags">
-                      <Icon name="labels"/>标签页</Link>
+                  <NavLink to="/tags" activeClassName="selected">
+                      <Icon name="labels"/>标签页</NavLink>
               </li>
               <li>
-                  <Link to="/money">
-                      <Icon name="money"/>记账页</Link>
+                  <NavLink to="/money" activeClassName="selected">
+                      <Icon name="money"/>记账页</NavLink>
               </li>
               <li>
-                  <Link to="/statistics">
-                      <Icon name="statistics" />统计页</Link>
+                  <NavLink to="/statistics" activeClassName="selected">
+                      <Icon name="statistics" />统计页</NavLink>
               </li>
           </ul>
       </NavWrapper>
