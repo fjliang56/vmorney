6350872fc227c547da634c3af5fe2df2b78facd6
fjliang56 <86655295+fjliang56@users.noreply.github.com>
2021-08-06 20:05:19 +0800

    Money.tsx 之 类型 UI 完成

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -40,6 +40,25 @@ const NotesSection = styled.section`
     }
 `;
 const CategorySection = styled.section`
+  font-size: 24px; 
+    > ul{
+      display: flex;
+      background: #C4C4C4;
+      > li {
+        width: 50%;text-align: center;padding: 16px 0;
+        position: relative;
+        &.selected::after{
+          content: '';
+          display: block;
+          height: 3px;
+          background: #333;
+          position: absolute;
+          bottom:0;
+          width: 100%;
+          left: 0;
+        }
+      }
+    }
 
 `;
 
