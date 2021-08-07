634d89e73c043b942dc1243820e89a34d010e992
fjliang56 <86655295+fjliang56@users.noreply.github.com>
2021-08-07 23:29:41 +0800

    删除 tag

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -6,29 +6,39 @@ const defaultTags = [
     {id: createId(), name: '食'},
     {id: createId(), name: '住'},
     {id: createId(), name: '行'},
-]
+];
 
 const useTags = () => {
-    const [tags, setTags] = useState<{id:number;name:string}[]>(defaultTags);
-    const findTag = (id: number) => tags.filter(tag=>tag.id===id)[0];
+    const [tags, setTags] = useState<{ id: number; name: string }[]>(defaultTags);
+    const findTag = (id: number) => tags.filter(tag => tag.id === id)[0];
     const findTagIndex = (id: number) => {
-        let result = -1
-      for(let i=0; i < tags.length; i++){
-          if(tags[i].id===id){
-              result = i;
-              break;
-          }
-      }
-      return result;
-    }
-    const updateTag = (id: number,obj: {name:string}) => {
+        let result = -1;
+        for (let i = 0; i < tags.length; i++) {
+            if (tags[i].id === id) {
+                result = i;
+                break;
+            }
+        }
+        return result;
+    };
+    const updateTag = (id: number, obj: { name: string }) => {
+        // 获取要改的 tag 的下标
         const index = findTagIndex(id);
         // 深拷贝 tags 得到tagsClone
-        const tagsClone = JSON.parse(JSON.stringify(tags))
+        const tagsClone = JSON.parse(JSON.stringify(tags));
         // 把 tagsClone 的第 index 删掉，换成 {id: id, name: obj.name}
-        tagsClone.splice(index, 1, {id:id, name: obj.name})
+        tagsClone.splice(index, 1, {id: id, name: obj.name});
         setTags(tagsClone);
-    }
-    return{tags, setTags,findTag,updateTag,findTagIndex}
+    };
+    const deleteTag = (id: number) => {
+        // 获取要改的 tag 的下标
+        const index = findTagIndex(id);
+        // 深拷贝 tags 得到tagsClone
+        const tagsClone = JSON.parse(JSON.stringify(tags));
+        // 把 tagsClone 的 index 删掉
+        tagsClone.splice(index, 1);
+        setTags(tagsClone);
+    };
+    return {tags, setTags, findTag, updateTag, findTagIndex,deleteTag};
 };
-export {useTags}
+export {useTags};
