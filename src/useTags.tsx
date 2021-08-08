b467bb101e26c2ba1fd21e85e64ceb2b1936cd7a
fjliang56 <86655295+fjliang56@users.noreply.github.com>
2021-08-08 10:37:50 +0800

    tags 数据持久化

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -1,15 +1,24 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import {createId} from './lib/createId';
-
-const defaultTags = [
-    {id: createId(), name: '衣'},
-    {id: createId(), name: '食'},
-    {id: createId(), name: '住'},
-    {id: createId(), name: '行'},
-];
+import {useUpdate} from './hooks/useUpdate';
 
 const useTags = () => {
-    const [tags, setTags] = useState<{ id: number; name: string }[]>(defaultTags);
+    const [tags, setTags] = useState<{ id: number; name: string }[]>([]);
+    useEffect(() => {
+        let localTags = JSON.parse(window.localStorage.getItem('tags') || '[]');
+        if (localTags.length === 0) {
+            localTags = [
+                {id: createId(), name: '衣'},
+                {id: createId(), name: '食'},
+                {id: createId(), name: '住'},
+                {id: createId(), name: '行'},
+            ];
+        }
+        setTags(localTags);
+    }, []);
+    useUpdate(() => {
+        window.localStorage.setItem('tags', JSON.stringify(tags));
+    }, [tags]);
     const findTag = (id: number) => tags.filter(tag => tag.id === id)[0];
     const findTagIndex = (id: number) => {
         let result = -1;
@@ -25,8 +34,14 @@ const useTags = () => {
         setTags(tags.map(tag => tag.id !== id ? {id, name: name} : tag));
     };
     const deleteTag = (id: number) => {
-        setTags(tags.filter(tag => tag.id !== id))
+        setTags(tags.filter(tag => tag.id !== id));
     };
-    return {tags, setTags, findTag, updateTag, findTagIndex,deleteTag};
+    const addTag = () => {
+        const tagName = window.prompt('新标签的名称为');
+        if (tagName !== null && tagName !== '') {
+            setTags([...tags, {id: createId(), name: tagName}]);
+        }
+    };
+    return {tags, setTags, findTag, updateTag, findTagIndex, deleteTag, addTag};
 };
 export {useTags};
